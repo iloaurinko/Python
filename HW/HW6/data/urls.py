253b91237d9e8d23a9e503c1d64506b8274a4555
SofiaPiep <sofiia.piepponen@gmail.com>
2024-10-27 15:46:18 +0200

    HW5

---
+++ HW/HW6/data/urls.py
@@ -0,0 +1,8 @@
+#создать класс URL
+#https://restful-booker.herokuapp.com/apidoc/index.html
+
+class Urls:
+    URL_AUTH = "https://restful-booker.herokuapp.com/auth"
+    URL = "https://restful-booker.herokuapp.com/booking"
+
+urls = Urls()
